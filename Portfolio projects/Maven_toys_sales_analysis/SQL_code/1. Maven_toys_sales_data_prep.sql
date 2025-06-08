9821d7708ad47bf43e5fc301ee3ab6a5f3bea26f
BenatrixB <benas.baranovskis98@gmail.com>
2025-06-08 15:17:26 +0300

    Added new files and folders
    Added new files and forlders for Maven_toys_sales data anlytics project.

---
+++ Portfolio projects/Maven_toys_sales_analysis/SQL_code/1. Maven_toys_sales_data_prep.sql
@@ -0,0 +1,8 @@
+-- Maven toys sales data prep
+-- In this query Mave toys sales data will be prepared and modelled for data visualization
+-- 1. DATA MUST BE IMPORTED FROM CSV FILE AND THE SCHEMA MUST BE CONNECTED
+-- products table imported
+SELECT * FROM products;
+-- sales table imported
+SELECT * FROM sales;
+
